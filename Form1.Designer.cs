616f6415921a93d4b1dd8f71f38107aac0d77fe4
rafaeldelgado <rafaeldelgado@RAFAELDELGA2C49>
2023-03-08 20:33:50 -0500

    Main menu images and labes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,12 +31,21 @@ namespace Lab_3___Wakey_Wakey_Coffee_Shop
         {
             this.label1 = new System.Windows.Forms.Label();
             this.storeName = new System.Windows.Forms.Label();
-            this.foodButton = new System.Windows.Forms.Button();
-            this.drinkButton = new System.Windows.Forms.Button();
-            this.dessertButton = new System.Windows.Forms.Button();
-            this.cartButton = new System.Windows.Forms.Button();
+            this.menuTable = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.foodLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.dessertLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.drinksLabel = new System.Windows.Forms.Label();
+            this.cartButton = new System.Windows.Forms.Button();
+            this.foodButton = new System.Windows.Forms.Button();
+            this.dessertButton = new System.Windows.Forms.Button();
+            this.drinkButton = new System.Windows.Forms.Button();
+            this.menuTable.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -58,85 +67,172 @@ namespace Lab_3___Wakey_Wakey_Coffee_Shop
             this.storeName.Text = "Wakey Wakey Coffee Shop";
             this.storeName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // foodButton
+            // menuTable
             // 
-            this.foodButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.foodButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.foodButton.Location = new System.Drawing.Point(71, 193);
-            this.foodButton.Name = "foodButton";
-            this.foodButton.Size = new System.Drawing.Size(123, 60);
-            this.foodButton.TabIndex = 2;
-            this.foodButton.Text = "Food";
-            this.foodButton.UseVisualStyleBackColor = true;
-            this.foodButton.Click += new System.EventHandler(this.foodButton_Click);
+            this.menuTable.ColumnCount = 3;
+            this.menuTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.menuTable.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.menuTable.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.menuTable.Controls.Add(this.tableLayoutPanel3, 2, 1);
+            this.menuTable.Controls.Add(this.cartButton, 2, 2);
+            this.menuTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menuTable.Location = new System.Drawing.Point(0, 0);
+            this.menuTable.Name = "menuTable";
+            this.menuTable.RowCount = 3;
+            this.menuTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.menuTable.Size = new System.Drawing.Size(800, 450);
+            this.menuTable.TabIndex = 6;
             // 
-            // drinkButton
+            // tableLayoutPanel1
             // 
-            this.drinkButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.drinkButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.drinkButton.Location = new System.Drawing.Point(604, 193);
-            this.drinkButton.Name = "drinkButton";
-            this.drinkButton.Size = new System.Drawing.Size(123, 60);
-            this.drinkButton.TabIndex = 3;
-            this.drinkButton.Text = "Drinks";
-            this.drinkButton.UseVisualStyleBackColor = true;
-            this.drinkButton.Click += new System.EventHandler(this.drinkButton_Click);
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.foodLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.foodButton, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 153);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(260, 144);
+            this.tableLayoutPanel1.TabIndex = 7;
             // 
-            // dessertButton
+            // foodLabel
             // 
-            this.dessertButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.dessertButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.dessertButton.Location = new System.Drawing.Point(337, 193);
-            this.dessertButton.Name = "dessertButton";
-            this.dessertButton.Size = new System.Drawing.Size(123, 60);
-            this.dessertButton.TabIndex = 4;
-            this.dessertButton.Text = "Desserts";
-            this.dessertButton.UseVisualStyleBackColor = true;
-            this.dessertButton.Click += new System.EventHandler(this.dessertButton_Click);
+            this.foodLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.foodLabel.AutoSize = true;
+            this.foodLabel.Location = new System.Drawing.Point(114, 131);
+            this.foodLabel.Name = "foodLabel";
+            this.foodLabel.Size = new System.Drawing.Size(31, 13);
+            this.foodLabel.TabIndex = 3;
+            this.foodLabel.Text = "Food";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.dessertButton, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.dessertLabel, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(269, 153);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(260, 144);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // dessertLabel
+            // 
+            this.dessertLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dessertLabel.AutoSize = true;
+            this.dessertLabel.Location = new System.Drawing.Point(108, 131);
+            this.dessertLabel.Name = "dessertLabel";
+            this.dessertLabel.Size = new System.Drawing.Size(43, 13);
+            this.dessertLabel.TabIndex = 5;
+            this.dessertLabel.Text = "Dessert";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.drinkButton, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.drinksLabel, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(535, 153);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(262, 144);
+            this.tableLayoutPanel3.TabIndex = 9;
+            // 
+            // drinksLabel
+            // 
+            this.drinksLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.drinksLabel.AutoSize = true;
+            this.drinksLabel.Location = new System.Drawing.Point(112, 131);
+            this.drinksLabel.Name = "drinksLabel";
+            this.drinksLabel.Size = new System.Drawing.Size(37, 13);
+            this.drinksLabel.TabIndex = 4;
+            this.drinksLabel.Text = "Drinks";
             // 
             // cartButton
             // 
             this.cartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cartButton.Location = new System.Drawing.Point(713, 415);
+            this.cartButton.Location = new System.Drawing.Point(720, 415);
             this.cartButton.Name = "cartButton";
-            this.cartButton.Size = new System.Drawing.Size(75, 23);
+            this.cartButton.Size = new System.Drawing.Size(77, 32);
             this.cartButton.TabIndex = 5;
             this.cartButton.Text = "Cart";
             this.cartButton.UseVisualStyleBackColor = true;
             this.cartButton.Click += new System.EventHandler(this.cartButton_Click);
             // 
-            // tableLayoutPanel1
+            // foodButton
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.foodButton, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.drinkButton, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.dessertButton, 1, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
-            this.tableLayoutPanel1.TabIndex = 6;
+            this.foodButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.foodButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.foodButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.foodButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.foodButton.Image = global::Lab_3___Wakey_Wakey_Coffee_Shop.Properties.Resources.foodMenuPhoto;
+            this.foodButton.Location = new System.Drawing.Point(3, 3);
+            this.foodButton.Name = "foodButton";
+            this.foodButton.Size = new System.Drawing.Size(254, 125);
+            this.foodButton.TabIndex = 2;
+            this.foodButton.UseVisualStyleBackColor = true;
+            this.foodButton.Click += new System.EventHandler(this.foodButton_Click);
+            // 
+            // dessertButton
+            // 
+            this.dessertButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.dessertButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.dessertButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dessertButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.dessertButton.Image = global::Lab_3___Wakey_Wakey_Coffee_Shop.Properties.Resources.dessertMenuPhoto;
+            this.dessertButton.Location = new System.Drawing.Point(3, 3);
+            this.dessertButton.Name = "dessertButton";
+            this.dessertButton.Size = new System.Drawing.Size(254, 125);
+            this.dessertButton.TabIndex = 4;
+            this.dessertButton.UseVisualStyleBackColor = true;
+            this.dessertButton.Click += new System.EventHandler(this.dessertButton_Click);
+            // 
+            // drinkButton
+            // 
+            this.drinkButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.drinkButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.drinkButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drinkButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.drinkButton.Image = global::Lab_3___Wakey_Wakey_Coffee_Shop.Properties.Resources.drinksMenuPhoto;
+            this.drinkButton.Location = new System.Drawing.Point(3, 3);
+            this.drinkButton.Name = "drinkButton";
+            this.drinkButton.Size = new System.Drawing.Size(256, 125);
+            this.drinkButton.TabIndex = 3;
+            this.drinkButton.UseVisualStyleBackColor = true;
+            this.drinkButton.Click += new System.EventHandler(this.drinkButton_Click);
             // 
             // mainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cartButton);
             this.Controls.Add(this.storeName);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuTable);
             this.Name = "mainMenu";
             this.Text = "Wakey Wakey Coffee Shop";
+            this.menuTable.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,7 +246,13 @@ namespace Lab_3___Wakey_Wakey_Coffee_Shop
         private System.Windows.Forms.Button drinkButton;
         private System.Windows.Forms.Button dessertButton;
         private System.Windows.Forms.Button cartButton;
+        private System.Windows.Forms.TableLayoutPanel menuTable;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label foodLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label dessertLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label drinksLabel;
     }
 }
 
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,9 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace Lab_3___Wakey_Wakey_Coffee_Shop.Properties
-{
+namespace Lab_3___Wakey_Wakey_Coffee_Shop.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -18,52 +19,74 @@ namespace Lab_3___Wakey_Wakey_Coffee_Shop.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lab_3___Wakey_Wakey_Coffee_Shop.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap dessertMenuPhoto {
+            get {
+                object obj = ResourceManager.GetObject("dessertMenuPhoto", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap drinksMenuPhoto {
+            get {
+                object obj = ResourceManager.GetObject("drinksMenuPhoto", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap foodMenuPhoto {
+            get {
+                object obj = ResourceManager.GetObject("foodMenuPhoto", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
